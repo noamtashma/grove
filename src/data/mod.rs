31d4bed616629f5f503dd4f770cd6883a7dd299d
Noam Ta Shma <noam.tashma@gmail.com>
2021-04-18 20:28:09 +0300

    more documentation

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,8 +1,5 @@
 pub mod example_data;
 
-/// this trait represents the data that will be stored inside the tree.
-/// the data can include: keys, values, indices, heights, sizes, sums maximums and minimums of subtrees, actions to be performed on the subtrees,
-/// and whatever your heart desires for your data structure needs.
 pub trait Data {
 	/// rebuild the associated data from the previous data and the sons.
 	fn rebuild_data<'a>(&'a mut self, left : Option<&'a Self>, right : Option<&'a Self>);
@@ -26,15 +23,52 @@ pub trait Data {
 }
 
 // TODO: remove Eq
+/// This trait represents the data that will be stored inside the tree.
+///
+/// Every node in the tree will contain an action, and a value.
+/// The action will be of type `Self`, and the value will be of type `Self::Value`.
+///
+/// `Self::Value` can include: keys, values, indices, heights, sizes, sums maximums
+/// and minimums of subtrees, and more. It is the type of values and summaries of values
+/// you can have in your tree.
+///
+/// `Self` is the type of actions that can be performed on the subtrees. for example,
+/// reverse a subtree, add a constant to a subtree, apply `max` with a constant on a subtree,
+/// and so on.
 pub trait Action : Copy + Eq {
-	// compose right to left. i.e., what chronologically happens first, is on the right.
+	/// Action composition. i.e., applying the resulting action should be equivalent
+	/// to applying the `other` function and then the `self` action.
+	/// Therefore, this composition must be associative. 
+	/// Compose right to left. i.e., what chronologically happens first, is on the right.
 	fn compose_a(self, other : Self) -> Self;
+	/// The identity action.
 	const IDENTITY : Self;
 
 	type Value : Copy;
+	/// Value composition. This is used to create the summary values
+	/// That give information about whole subtrees.
+	/// Since the structure of the tree may be any structure,
+	/// but the summary value should depend on the values in the subtree and
+	/// not on the tree structure, this composition must be associative.
 	fn compose_v(left : Self::Value, right : Self::Value) -> Self::Value;
 	const EMPTY : Self::Value;
+
 	// default implementation that does nothing:
+	/// Applying an action to a value.
+	/// The default implementation just returns the value, since
+	/// this is always a legal implementation.
+	///
+	/// Applying an action on a summary value must be equal to applying the
+	/// action separately and then summing the values:
+	/// ```
+	///    action.act(Self::compose_v(value_1, value_2))
+	///    == Self::compose_v(action.act(value_1), action.act(value_2))
+	/// ```
+	/// Indeed, this property is used so that the summary values can be updated without
+	/// updating the whole tree.
+	///
+	/// Therefore, This is essentially a monoid action by the monoid of actions
+	/// on the monoid of values.
 	fn act(self, other : Self::Value) -> Self::Value {
 		other
 	}
