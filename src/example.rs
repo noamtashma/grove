4ea631a21cd7d40e9f80aba1a89b4e2b77ac9a66
Noam Ta Shma <noam.tashma@gmail.com>
2021-04-19 09:08:22 +0300

    keep the current horrible example code for posterity

--- src/example.rs
+++ src/example.rs
@@ -7,31 +7,62 @@ use super::trees::*;
 use super::data::*;
 use super::data::example_data::*;
 
+const MODULUS : I = 1_000_000_000;
+
 // an example:
 
 
+type I = i64;
 
 // inclusive intervals
 #[derive(Clone, Copy, Debug)]
-enum Interval {
-    Increasing(usize , usize),
-    Decreasing(usize, usize),
+
+// either increasing or decreasing intervals. 
+// i.e., {start:3, end:7} is [3,4,5,6,7],
+// and {start:7, end:4} is [7,6,5,4]
+struct Interval {
+    start : I,
+    end : I,
 }
 
 impl Interval {
     fn size(&self) -> usize {
-        unimplemented!()
+        1 + if self.end > self.start { self.end - self.start } else { self.start - self.end } as usize
     }
 
     fn flip(&mut self) {
-        unimplemented!()
+        std::mem::swap(&mut self.start, &mut self.end);
     }
-    fn sum_with_index(&self, index : usize) -> usize {
-        unimplemented!()
+    
+    // sum of i*A[i] if the array only starts at index `index` 
+    fn sum_with_index(&self, index2 : usize) -> I {
+        let index = (index2 as I) % MODULUS;
+        let inter = *self;
+        let size = (inter.size() as I) % MODULUS;
+        // denote i = index + j
+        // i*A[i] = (index + j) * (inter.start + incr*j) = incr*j^2 + j*(incr*index + inter.start) + index*inter.start
+        // sum (i < n) i^2 = n*(n-1)*(2n-1)/6
+        // sum (i < n) i = n*(n-1)/2
+        let a = (size*(size-1) % MODULUS)*(2*size-1)/6;
+        let b = if self.start < self.end {inter.start + index} else {inter.start - index} % MODULUS;
+        let c = index*(inter.start % MODULUS) % MODULUS;
+
+        let x = (size*(size-1)/2) % MODULUS;
+
+        let res = if self.start < self.end {a + b*x + c*size} else { b*x + c*size - a};
+        return res % MODULUS;
     }
 
-    fn split_at_index(&self, index : usize) -> (Interval, Interval) {
-        unimplemented!()
+    // split into the index first values and the rest. i.e.,
+    // spliting [6,5,4] with index=1 gives [6], [5,4]
+    fn split_at_index(&self, index2 : usize) -> (Interval, Interval) {
+        let index = index2 as I;
+        assert!(0 < index2 && index2 < self.size());
+        if self.start <= self.end {
+            (Interval {end : self.start + index - 1, ..*self}, Interval {start : self.start + index, ..*self})
+        } else {
+            (Interval {end : self.start - index + 1, ..*self}, Interval {start : self.start - index, ..*self})
+        }
     }
 }
 
@@ -39,9 +70,9 @@ impl Interval {
 struct RAValue {
     size : usize,
     start_index : usize,
-    sum : usize,
+    sum : I,
     // sum of i*a[i] for i in [start_index .. start_index+size]
-    index_sum : usize,
+    index_sum : I,
     interval : Interval, // always with the same size. has meaning only in node values.
 }
 
@@ -56,14 +87,14 @@ impl RAValue {
         }
     }
 
+    // TODO - this doesn't update the values!
     pub fn split_at_index(&self, index: usize) -> (Self, Self) {
         assert!(self.interval.size() > 0); // this must be a node value
         assert!(index > self.start_index && index <= self.start_index + self.size);
-        let mut v1 = *self;
-        let mut v2 = *self;
+
         let (i1, i2) = self.interval.split_at_index(index - self.start_index);
-        v1.interval = i1;
-        v2.interval = i2;
+        let v1 = RAValue::from_interval(self.start_index, i1);
+        let v2 = RAValue::from_interval(self.start_index + i1.size(), i2);
         (v1, v2)
     }
 }
@@ -78,11 +109,11 @@ struct RAAction {
     // i.e., the operation switches A[i] to A[i+c] for all i.
 
     // doesn't contain bounds because the action will be applied in the bounds only.
-    transformation : Result<usize, usize>,
+    transformation : Result<I, I>,
 }
 
 impl Action for RAAction {
-    type Value = RAValue;
+    type Summary = RAValue;
     fn compose_a(self, other : Self) -> RAAction {
         let new_transformation = match (self.transformation, other.transformation) {
             (Ok(a), Ok(b)) => Ok(a+b),
@@ -94,15 +125,15 @@ impl Action for RAAction {
     }
     const IDENTITY : Self = RAAction{transformation : Ok(0)};
     
-    fn compose_v(left : Self::Value, right : Self::Value) -> Self::Value {
+    fn compose_s(left : Self::Summary, right : Self::Summary) -> Self::Summary {
         assert!(left.size == 0 || right.size == 0 ||
             left.start_index + left.size == right.start_index); // TODO
         RAValue {
             size : left.size + right.size,
-            sum : left.sum + right.sum,
+            sum : left.sum + right.sum % MODULUS,
             start_index : if left.size != 0 {left.start_index} else {right.start_index}, // for the case of the empty value
-            index_sum : left.index_sum + right.index_sum,
-            interval : Interval::Increasing(0, 0), // some default interval
+            index_sum : left.index_sum + right.index_sum % MODULUS,
+            interval : Interval{start : 0, end : 0}, // some default interval
         }
     }
     const EMPTY : RAValue = RAValue {
@@ -110,20 +141,24 @@ impl Action for RAAction {
         start_index : 0, // random value
         sum : 0,
         index_sum : 0,
-        interval : Interval::Increasing(0, 0),
+        interval : Interval{start : 0, end : 0},
     };
 
-    fn act(self, mut val : Self::Value) -> Self::Value {
+    fn act(self, mut val : Self::Summary) -> Self::Summary {
         match self.transformation {
             Ok(a) => {
-                val.start_index += a;
-                val.index_sum += a*val.sum;
+                val.start_index = (val.start_index as I + a) as usize;
+                val.sum %= MODULUS; // why is this needed?
+                val.index_sum += ((a as I) % MODULUS)*val.sum;
+                val.index_sum %= MODULUS;
                 val
             },
             Err(a) => {
-                let end_index = val.start_index + val.size;
-                let new_start_index = a - end_index;
-                val.index_sum = (val.start_index + new_start_index)*val.sum - val.index_sum;
+                let end_index = val.start_index + val.size - 1;
+                let new_start_index = a as usize - end_index;
+                val.sum %= MODULUS; // why is this needed?
+                val.index_sum = ((a as I) % MODULUS)*val.sum - val.index_sum;
+                val.index_sum %= MODULUS;
                 val.start_index = new_start_index;
                 val.interval.flip();
                 val
@@ -131,6 +166,12 @@ impl Action for RAAction {
         }
     }
 
+    fn to_reverse(&self) -> bool {
+        match self.transformation {
+            Ok(_) => false,
+            Err(_) => true,
+        }
+    }
 }
 /*
 struct RAData {
@@ -185,19 +226,29 @@ impl SizedAction for RAAction {
     }
 }
 
+impl crate::data::Reverse for RAAction {
+    fn internal_reverse(node : &mut crate::trees::basic_tree::BasicNode<Self>) {
+        node.access(); // TODO
+        let RAValue {start_index, size, ..} = node.segment_summary();
+        let action = RAAction {transformation : Err((2*start_index + size - 1) as I)};
+        node.act(action);
+    }
+}
+
 impl SplayTree<RAAction> {
     // splits the tree - modifies the first tree and returns the second tree
     // splits to [0, index) and [index, length)
-    fn search_split<'a>(&'a mut self, mut index : usize) -> SplayTree<RAAction> {
+    // TODO: exmplain
+    fn search_split(&mut self, index1 : usize, index2 : usize) -> SplayTree<RAAction> {
 
-        let mut locator = locate_by_index_range(index, index);
+        let locator = locate_by_index_range(index1, index1);
         let mut walker = // using an empty range so that we'll only end up at a node
             // if we actually need to split that node
             search_by_locator(self, &locator).void_unwrap();
         
 
         if let Some(val) = walker.value_mut() { // if we need to split a node
-            let (v1, v2) = val.split_at_index(index);
+            let (v1, v2) = val.split_at_index(index2);
             *val = v1;
             next_empty(&mut walker).unwrap(); // not at an empty node
             walker.insert_new(v2).unwrap(); // the position must be empty
@@ -206,20 +257,83 @@ impl SplayTree<RAAction> {
         return walker.split().unwrap();
     }
 
+    /// Unites the two trees into one.
+    fn union(&mut self, other : Self) {
+        let mut walker = self.walker();
+        while let Ok(_) = walker.go_right()
+            {}
+        match walker.go_up() {
+            Err(()) => { // the tree is empty; just substiture the other tree.
+                drop(walker);
+                *self = other;
+                return;
+            },
+            Ok(b) => assert!(b == false),
+        };
+        walker.splay();
+        if let trees::basic_tree::BasicTree::Root(node) = walker.inner_mut() {
+            node.right = other.into_inner();
+            node.rebuild();
+            return;
+        }
+        else {
+            panic!();
+        }
+    }
+
     // reverse the segment [low, high)
     fn reverse_segment(&mut self, low : usize, high : usize) {
-        let mut self2 = self.search_split(low);
-        let self3 = self2.search_split(high);
-        //self2.root_data_mut().map(|r| r.reverse());
-        // TODO
+        let mut self2 = self.search_split(low, low);
+        // high-low and not high since this counts the index based on the split tree and not the original tree
+        let self3 = self2.search_split(high-low, high);
+        self2.reverse();
+        // unite back together
+        self2.union(self3);
+        self.union(self2);
     }
 }
 
+
+pub fn yarra(n : usize, k : usize) -> I {
+    let mut tree : SplayTree<RAAction> = SplayTree::new();
+    let inter = Interval {start : 0, end : (n-1) as I};
+    tree.walker().insert_new(RAValue::from_interval(0, inter)).unwrap();
+
+    let mut sn = 1;
+    let mut tn = 1;
+    for round in 0..k {
+        if round % 1000 == 999 {
+            //dbg!(tree.segment_value());
+            dbg!(round);
+        }
+
+        if sn < tn {
+            tree.reverse_segment(sn, tn+1);
+        } else if sn > tn {
+            tree.reverse_segment(tn, sn+1);
+        }
+
+        sn += tn;
+        sn %= n;
+        tn += sn;
+        tn %= n;
+    }
+
+    let res = tree.segment_value().index_sum;
+    res
+}
 
 
 pub fn test() {
     println!("Hello, world!");
 
+    dbg!(yarra(1000_000_000_000_000_000, 1000_000));
+    //dbg!(yarra(15, 10));
+    use std::io::Write;
+    use std::io::stdout;
+    stdout().flush().unwrap();
+
+    /*
     let mut tree : SplayTree<KeyAction<_>> = SplayTree::new();
     for x in 1..25 {
         insert_by_key(&mut tree, Key::new(x*5));
@@ -238,6 +352,7 @@ pub fn test() {
         }
     }
     println!("done!");
+    */
 }
 
 
